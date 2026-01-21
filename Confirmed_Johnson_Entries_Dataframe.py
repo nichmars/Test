7ad4adfcf52a795900ce82d606e74a33594ec0a5
TylerVandy <tylervandy13@gmail.com>
2026-01-21 16:44:42 -0500

    Add files via upload
    This python file simply creates a dataframe which excludes all entries that are not FULLY confirmed as true data. *IMPORTANT*: You may need to change your file path for uploading the excel file.

---
+++ Confirmed_Johnson_Entries_Dataframe.py
@@ -0,0 +1,34 @@
+# %%
+import pandas as pd
+
+j_df = pd.read_excel('Annual+DAF+Report+2025+Dataset.xlsx', sheet_name='data')
+
+
+j_df_conf = j_df.copy()
+
+j_df_conf.loc[j_df_conf['source_record'] == "IRS 990", 'source_record'] = "Confirmed"
+j_df_conf.loc[j_df_conf['source_record'] == "NPT Historical Data", 'source_record'] = "Confirmed"
+j_df_conf.loc[j_df_conf['source_record'] == "Paper Submission", 'source_record'] = "Confirmed"
+
+j_df_conf.loc[j_df_conf['source_number_of_accounts'] == "IRS 990", 'source_number_of_accounts'] = "Confirmed"
+j_df_conf.loc[j_df_conf['source_number_of_accounts'] == "NPT Historical Data", 'source_number_of_accounts'] = "Confirmed"
+j_df_conf.loc[j_df_conf['source_number_of_accounts'] == "Paper Submission", 'source_number_of_accounts'] = "Confirmed"
+
+j_df_conf.loc[j_df_conf['source_total_daf_contributions'] == "IRS 990", 'source_total_daf_contributions'] = "Confirmed"
+j_df_conf.loc[j_df_conf['source_total_daf_contributions'] == "NPT Historical Data", 'source_total_daf_contributions'] = "Confirmed"
+j_df_conf.loc[j_df_conf['source_total_daf_contributions'] == "Paper Submission", 'source_total_daf_contributions'] = "Confirmed"
+
+j_df_conf.loc[j_df_conf['source_total_daf_grants'] == "IRS 990", 'source_total_daf_grants'] = "Confirmed"
+j_df_conf.loc[j_df_conf['source_total_daf_grants'] == "NPT Historical Data", 'source_total_daf_grants'] = "Confirmed"
+j_df_conf.loc[j_df_conf['source_total_daf_grants'] == "Paper Submission", 'source_total_daf_grants'] = "Confirmed"
+
+j_df_conf.loc[j_df_conf['source_eoy_daf_assets'] == "IRS 990", 'source_eoy_daf_assets'] = "Confirmed"
+j_df_conf.loc[j_df_conf['source_eoy_daf_assets'] == "NPT Historical Data", 'source_eoy_daf_assets'] = "Confirmed"
+j_df_conf.loc[j_df_conf['source_eoy_daf_assets'] == "Paper Submission", 'source_eoy_daf_assets'] = "Confirmed"
+
+
+j_df_conf = j_df_conf[(j_df_conf['source_record'] == 'Confirmed') & (j_df_conf['source_number_of_accounts'] == 'Confirmed') & (j_df_conf['source_total_daf_contributions'] == 'Confirmed') & (j_df_conf['source_total_daf_grants'] == 'Confirmed') & (j_df_conf['source_eoy_daf_assets'] == 'Confirmed')]
+
+j_df_conf
+# %%
+_
