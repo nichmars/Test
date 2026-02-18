725202f53489f308dfa21eb3bac831a2fa21f44c
nichmars <nichmars@mail.gvsu.edu>
2026-02-18 15:24:38 -0500

    hm

--- Trying Random Stuff.py
+++ Trying Random Stuff.py
@@ -24,22 +24,22 @@ contributions = df["Contributions"]
 grants = df["Grants"]
 assets = df["Assets"]
 
-accounts_tensor = torch.tensor(accounts.values, dtype = torch.float32)
-contributions_tensor = torch.tensor(contributions.values, dtype = torch.float32)
-grants_tensor = torch.tensor(grants.values, dtype = torch.float32)
-assets_tensor = torch.tensor(assets.values, dtype=torch.float32)
-lr = .0001
-epochs = 1000
-x = torch.zeros(len(accounts_tensor))
+# accounts_tensor = torch.tensor(accounts.values, dtype = torch.float32)
+# contributions_tensor = torch.tensor(contributions.values, dtype = torch.float32)
+# grants_tensor = torch.tensor(grants.values, dtype = torch.float32)
+# assets_tensor = torch.tensor(assets.values, dtype=torch.float32)
+# lr = .0001
+# epochs = 1000
+# x = torch.zeros(len(accounts_tensor))
 
-for epoch in range(epochs):
-    loss = ( x - accounts_tensor)
+# for epoch in range(epochs):
+#     loss = ( x - accounts_tensor)
 
-    loss.backward()
+#     loss.backward()
 
-    with torch.no_grad:
-        x-= lr*x.grad
+#     with torch.no_grad:
+#         x-= lr*x.grad
 
-    x.grad.zero_()
-    if epoch == 0 % 10:
-        print(f"Step {epoch+1:02d}: x = {x.item():.4f}, loss = {loss.item():.6f}")
+#     x.grad.zero_()
+#     if epoch == 0 % 10:
+#         print(f"Step {epoch+1:02d}: x = {x.item():.4f}, loss = {loss.item():.6f}")
