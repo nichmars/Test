c78a019b0abf573ab7b3c8d156b8ef0d8f7e898d
nichmars <nichmars@mail.gvsu.edu>
2026-02-18 15:41:18 -0500

    Update Trying Random Stuff.py

--- Trying Random Stuff.py
+++ Trying Random Stuff.py
@@ -3,43 +3,50 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import torch
 
-df = pd.read_excel("Filtered Data with Subtypes Again.xlsx")
+# df = pd.read_excel("Filtered Data with Subtypes Again.xlsx")
 
-df_adj = df.shift(1)
-df_adj = df_adj.rename(columns={
-    'ein':'ein_2',
-    'Year':'Previous Year',
-    'Accounts':"Accounts_2",
-    "Contributions": "Contributions_2",
-    "Grants":"Grants_2",
-    "Assets": "Assets_2"
-})
-df_adj = df_adj.drop(columns=["Type", "subtype"])
-df_comb = pd.concat([df, df_adj], axis = 1)
+# df_adj = df.shift(1)
+# df_adj = df_adj.rename(columns={
+#     'ein':'ein_2',
+#     'Year':'Previous Year',
+#     'Accounts':'Accounts_2',
+#     'Contributions': 'Contributions_2',
+#     'Grants':'Grants_2',
+#     'Assets': 'Assets_2',
+# })
+# df_adj = df_adj.drop(columns=["Type", "subtype"])
+# df_comb = pd.concat([df, df_adj], axis = 1)
 
-df_comb.to_excel("Appened Years Dataset.xlsx")
-print(df_comb.columns)
+# df_comb.to_excel("Appened Years Dataset.xlsx")
+
+df = pd.read_excel("Appened Years Dataset.xlsx")
+
+df = df[df["Year"]==2019]
 accounts = df["Accounts"]
 contributions = df["Contributions"]
 grants = df["Grants"]
 assets = df["Assets"]
+accounts_2 = df["Accounts"]
+contributions_2 = df["Contributions"]
+grants_2 = df["Grants"]
+assets_2 = df["Assets"]
 
-# accounts_tensor = torch.tensor(accounts.values, dtype = torch.float32)
-# contributions_tensor = torch.tensor(contributions.values, dtype = torch.float32)
-# grants_tensor = torch.tensor(grants.values, dtype = torch.float32)
-# assets_tensor = torch.tensor(assets.values, dtype=torch.float32)
-# lr = .0001
-# epochs = 1000
-# x = torch.zeros(len(accounts_tensor))
+accounts_tensor = torch.tensor(accounts.values, dtype = torch.float32)
+contributions_tensor = torch.tensor(contributions.values, dtype = torch.float32)
+grants_tensor = torch.tensor(grants.values, dtype = torch.float32)
+assets_tensor = torch.tensor(assets.values, dtype=torch.float32)
+lr = .0001
+epochs = 1000
+x = torch.zeros(len(accounts_tensor))
 
-# for epoch in range(epochs):
-#     loss = ( x - accounts_tensor)
+for epoch in range(epochs):
+    loss = ( x - accounts_tensor)
 
-#     loss.backward()
+    loss.backward()
 
-#     with torch.no_grad:
-#         x-= lr*x.grad
+    with torch.no_grad:
+        x-= lr*x.grad
 
-#     x.grad.zero_()
-#     if epoch == 0 % 10:
-#         print(f"Step {epoch+1:02d}: x = {x.item():.4f}, loss = {loss.item():.6f}")
+    x.grad.zero_()
+    if epoch == 0 % 10:
+        print(f"Step {epoch+1:02d}: x = {x.item():.4f}, loss = {loss.item():.6f}")
